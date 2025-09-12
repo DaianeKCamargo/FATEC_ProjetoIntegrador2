4c337135296aeae59e0016c3969dfa892fd928e6
Daiane Camargo <dkeelly@gmail.com>
2025-09-11 22:40:31 -0300

    criacao components e pages

---
+++ tampets/src/Components/Navegation.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+export function Navegation() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
---
+++ tampets/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import { Navegation } from "@/Components/Navegation";
+
+export default function Home(){
+    return(
+        <>
+        <Navegation></Navegation>  
+        </>
+    );
+}
