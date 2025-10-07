97b0e1cb3d874742f9c72c40b40c9fbb6ef2b4e0
Daiane Camargo <dkeelly@gmail.com>
2025-10-06 21:59:13 -0300

    Criação de Nav Bar
    Tentativa de criar uma nav bar ... sem sucesso ainda

--- tampets/src/Components/Navegation.tsx
+++ tampets/src/Components/Navegation.tsx
@@ -1,20 +1,67 @@
 'use client';
 
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button, NavDropdown } from "react-bootstrap";
+import logoHome from "@/assets/logoHome.png"
 
 export function Navegation() {
+  
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand, idx) => (
+        <Navbar className="bg-body-tertiary mb-3" key={String(expand) || idx} expand={expand}>
+          <Container fluid>
+{/* LOGO/HOME */}
+            <Navbar.Brand href="#home">
+              <img
+                src={logoHome.src}
+                width="520"
+                height="auto"
+                className="d-inline-block align-top"
+                alt="logo Tampets"
+              />
+              </Navbar.Brand>
+
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <NavDropdown
+                      title="O Projeto"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                    >
+                      <NavDropdown.Item href="#action3">Quem Somos</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action4">Conheça o Projeto</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action5">TamPets na Mídia</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action4">Galeria</NavDropdown.Item>
+                    </NavDropdown>
+                
+{/* RELATÓRIO */}
+              
+                  <NavDropdown
+                    title="O Projeto"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Dados do Projeto</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action4">CO2</NavDropdown.Item>
+                  </NavDropdown>
+    
+{/* PONTOS DE COLETA */}
+                    <Nav.Link href="#action1">Pontos de Coleta</Nav.Link>
+
+{/* DOAÇÕES */}
+                    <Nav.Link href="#action2">Doações</Nav.Link>
+
+{/* PARCEIROS */}
+                    <Nav.Link href="#action2">Parceiros</Nav.Link>
+  
+                  </Nav>
+                 
+                    <Button variant="outline-success"> Login | Cadastro </Button>
+
+          </Container>
+        </Navbar>
+  ))}
     </>
-  );
+  )
 }
