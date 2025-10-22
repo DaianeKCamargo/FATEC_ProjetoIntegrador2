50e406ef502c61f826a177454c7de89281022a15
SabrisDev <sah.silva1d29@gmail.com>
2025-10-21 21:49:16 -0300

    criando paginas da galeria

---
+++ tampets/src/app/oProjeto/galeriaFotos/galeriaEmpresas/page.tsx
@@ -0,0 +1,33 @@
+export default function GaleriaEmpresas() {
+    return (
+      <section>
+        <h1 style={{ textAlign: "center", marginBottom: "20px", marginTop: "20px" }}>
+            TamPets nas Empresas 📸</h1>
+
+        <div style={{ display: "flex", gap: "20px", flexWrap: "wrap", justifyContent: "center" }}>
+          <CardDesenhado
+            imagem={"/coleta.PNG"}
+            titulo="Pontos de Coleta"
+            descricao="Pontos de Coleta do Projeto!"
+          />
+          <CardDesenhado
+            imagem={"/tampetszf.jpg"}
+            titulo="Empresas"
+            descricao="TamPets nas Empresas!"
+          /> 
+
+          <CardDesenhado
+            imagem={"/evento.PNG"}
+            titulo="Eventos"
+            descricao="Eventos do Projeto!"
+          />
+
+          <CardDesenhado
+            imagem={"/tampets112023.webp"}
+            titulo="Escolas"
+            descricao="Escolas que Participam!"
+          />
+        </div>
+      </section>
+    )
+}
--- tampets/src/app/oProjeto/naMidia/page.tsx
+++ tampets/src/app/oProjeto/naMidia/page.tsx
@@ -1,5 +1,7 @@
 export default function naMidia() {
     return (
         <h1>Página: Na Mídia</h1>
+
+        
     )
 }
