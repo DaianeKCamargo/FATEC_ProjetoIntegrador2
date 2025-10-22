8fbb6da48e86d315f1194751732f4d4121322a58
SabrisDev <sah.silva1d29@gmail.com>
2025-10-21 21:13:40 -0300

    ajeitando galeria
    ajustes da galeria

--- tampets/src/app/oProjeto/galeriaFotos/page.tsx
+++ tampets/src/app/oProjeto/galeriaFotos/page.tsx
@@ -2,21 +2,33 @@ import CardDesenhado from "@/Components/CardDesenhado";
 
 export default function GaleriaFotos() {
     return (
-        <section>
-        <h1>Página: Galeria de Fotos</h1>
+      <section>
+        <h1 style={{ textAlign: "center", marginBottom: "20px", marginTop: "20px" }}>Galeria TamPets 📸</h1>
 
-         <div style={{ display: "flex", gap: "20px", flexWrap: "wrap", justifyContent: "center" }}>
-      <CardDesenhado
-        imagem={"/public/coleta.PNG"}
-        titulo="Pontos de Coleta"
-        descricao="Pontos de Coleta do Projeto!"
-      />
-      <CardDesenhado
-        imagem={"/public/tampetszf.jpg"}
-        titulo="Empresas"
-        descricao="TamPets nas Empresas!"
-      />
-    </div>
-    </section>
+        <div style={{ display: "flex", gap: "20px", flexWrap: "wrap", justifyContent: "center" }}>
+          <CardDesenhado
+            imagem={"/coleta.PNG"}
+            titulo="Pontos de Coleta"
+            descricao="Pontos de Coleta do Projeto!"
+          />
+          <CardDesenhado
+            imagem={"/tampetszf.jpg"}
+            titulo="Empresas"
+            descricao="TamPets nas Empresas!"
+          /> 
+
+          <CardDesenhado
+            imagem={"/evento.PNG"}
+            titulo="Eventos"
+            descricao="Eventos do Projeto!"
+          />
+
+          <CardDesenhado
+            imagem={"/tampets112023.webp"}
+            titulo="Escolas"
+            descricao="Escolas que Participam!"
+          />
+        </div>
+      </section>
     )
 }
