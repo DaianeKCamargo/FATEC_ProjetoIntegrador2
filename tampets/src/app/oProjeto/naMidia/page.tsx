bbaf6a6a78ee9a1c6cacc46d64978b28b4ce233c
SabrisDev <sah.silva1d29@gmail.com>
2025-10-22 22:05:44 -0300

    Update TamPets na mídia
    criação do título e subtítulo

--- tampets/src/app/oProjeto/naMidia/page.tsx
+++ tampets/src/app/oProjeto/naMidia/page.tsx
@@ -1,6 +1,12 @@
 export default function naMidia() {
     return (
-        <h1>Página: Na Mídia</h1>
+       <section>
+        <div style={{ textAlign: "center", marginBottom: "20px", 
+                marginTop: "20px", padding: "2px" }}>
+                <h1>TamPets na Mídia 📺</h1>
+                <h2>Confira as aparições da TamPets na mídia!</h2>
+        </div>
+       </section>
 
         
     )
