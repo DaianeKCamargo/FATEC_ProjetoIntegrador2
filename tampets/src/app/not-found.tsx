4778c14636c4cb1ccce764fba65e5a00b8da864a
BOLZIRI <nicole.oliveira12@fatec.sp.gov.br>
2025-10-23 20:58:18 -0300

    início de uma página not found

--- tampets/src/app/not-found.tsx
+++ tampets/src/app/not-found.tsx
@@ -1,13 +1,14 @@
-// Página not found (404) -> toda vez que o usuário tentar acessar uma rota inexistente no sistema, ele será redirecionado para essa página.
-
 import Link from "next/link";
 
 export default function NotFound() {
     return (
         <div>
-            <h1> ERRO 404 - PÁGINA NÃO ENCONTRADA </h1>
-            <p> A página que está tentando acessar não existe! </p>
-            <Link href="/"> Voltar para à página Home </Link>
+            <h1> Erro 404 - Página não encontrada! </h1>
+
+            <p> Links que podem ser úteis: </p>
+            <Link href="/pontoColeta"> Encontre Pontos de Coleta perto de você! </Link>
+            <br /> <Link href="/comoDoar"> Está com dúvidas sobre como doar? Clique aqui </Link>
+            <br /> <Link href="/"> Faça parte do projeto, junte-se a nós! </Link>
         </div>
     )
 }
