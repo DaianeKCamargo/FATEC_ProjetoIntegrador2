e8e130a59fc56149915c67c6af038ca038dbf2a9
SabrisDev <sah.silva1d29@gmail.com>
2025-10-23 20:40:34 -0300

    Criação SearchBar e implementação
    Criei o componente da barra de pesquisa e o style dela e adicionei na page ponto de coleta.

--- tampets/src/app/pontoColeta/page.tsx
+++ tampets/src/app/pontoColeta/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+import SearchBar from "@/Components/SearchBar";
+
+
 export default function pontoColeta() {
+    const handleSearch = (query: string) => {
+    console.log("Busca:", query);
+  };
     return (
-        <h1>Página: Ponto de Coleta</h1>
+         <section  style={{
+        display: "flex",
+        justifyContent: "center", // alinha horizontalmente
+        alignItems: "center",     // alinha verticalmente
+        flexDirection: "column",  // empilha verticalmente
+        gap: "20px",              // espaçamento entre elementos
+        marginTop: "20px"         
+      }}
+    >
+            <h2>Pontos de Coleta ♻️</h2>
+            <h3>Encontre o ponto de coleta mais próximo de você!</h3>
+            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                 <SearchBar placeholder="Buscar ponto de coleta..." onSearch={handleSearch} />
+            </div>
+         </section>
     )
 }
+                
+        
