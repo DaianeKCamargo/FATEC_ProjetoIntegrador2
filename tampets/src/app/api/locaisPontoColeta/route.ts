1441d73fa3ac6fcf7bc830f5682402273b4cc2a0
SabrisDev <sah.silva1d29@gmail.com>
2025-10-23 21:22:23 -0300

    Update route.ts

--- tampets/src/app/api/locaisPontoColeta/route.ts
+++ tampets/src/app/api/locaisPontoColeta/route.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 
-export function GET {
+export function GET() {
     return NextResponse.json ([
         {
-            
+            nome_estabelecimento: "Ponto de Coleta Central",
+            endereco: "Rua Principal, 123, Centro, Sorocaba - SP",
+            telefone: "(11) 1234-5678",
+            horario_funcionamento: "Segunda a Sexta: 8h - 18h; Sábado: 9h - 14h"
+        },
+        {
+            nome_estabelecimento: "EcoPonto Verde",
+            endereco: "Avenida das Flores, 456, Jardim das Acácias, Sorocaba - SP",
+            telefone: "(11) 9876-5432",
+            horario_funcionamento: "Segunda a Sexta: 8h - 18h; Sábado: 9h - 14h"
         }
-    ]
-    )
+    ], {status: 200})
 }
