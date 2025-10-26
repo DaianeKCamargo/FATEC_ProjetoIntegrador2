9fa130ebd00e81e10e00bd3ff389c78cf8fc6561
DaianeKCamargo <dkeelly@gmail.com>
2025-10-25 21:15:09 -0300

    Alteração do caminho principal
    Notei um erro, onde a pasta principal é a fatec_projetointegrador2, mas ai tinha uma outra pasta tampets dando problema sempre na hora de fazer instalação e linkagem das páginas, então removi ela onde a página principal original esta com todos os arquivos.

---
+++ src/Components/NavegationBar.tsx
@@ -0,0 +1,171 @@
+'use client';
+
+import Link from "next/link";
+import Image from "next/image";
+import NavItem, { NavItemInterface } from "./NavItem";
+import { usePathname } from "next/navigation";
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+import { useState } from "react";
+
+// Objeto de navegação da Navbar
+export function NavegationBar() {
+    const items: NavItemInterface[] = [
+        {
+            url: "/oProjeto",
+            label: "O Projeto"
+        },
+        {
+            url: "/relatorio",
+            label: "Relatório"
+        },
+        {
+            url: "/pontoColeta",
+            label: "Ponto de Coleta"
+        },
+        {
+            url: "/comoDoar",
+            label: "Como Doar"
+        },
+        {
+            url: "/parceiros",
+            label: "Parceiros"
+        },
+    ]
+
+    // Hook para pegar a rota atual
+    const pathname = usePathname();
+
+    // Estado para controlar o menu mobile
+    const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+    return (
+        <header>
+            <nav className="navbar">
+
+                <Link href="/">
+                    <Image className="logo" src="/logoTampets2.png" alt="Logo Tampets" width={300} height={300} />
+                </Link>
+
+                <ul className={`nav-items ${openMenu ? 'open' : ''}`}>
+                    {items.map((item, index) => (
+                        <NavItem
+                            key={index}
+                            url={item.url}
+                            label={item.label}
+                            isActive={pathname === item.url}
+                        />
+                    ))}
+                </ul>
+
+                <button className="menuButton" onClick={() => setOpenMenu(!openMenu)}>
+                    {openMenu ? <FaXmark /> : <FaBarsStaggered />}
+                </button>
+
+                <button className="loginCadastro" onClick={() => {
+                window.location.href = "/cadastrar";
+            }}>
+                    Login | Cadastro
+                </button>
+            </nav>
+
+
+            {/* Usando styled-jsx para que o TypeScript não exija um arquivo de declaração .css */}
+            <style jsx>
+                {` 
+                    .navbar {
+                        display: flex;
+                        align-items: flex-start;
+                        justify-content: space-around;
+                        background: #fff;
+                        box-shadow: 0 1px 3px rgba(0,0,0,0.08);
+                        padding: 0;
+                    } 
+
+                    .nav-items {
+                        list-style: none;
+                        display: flex;
+                        gap: 20px;
+                        margin: 0;
+                        align-items: center;
+                        font-size: 18px;
+                        margin-top: 10px; 
+                    }
+
+                    .nav-items.open {
+                        flex-direction: column;
+                        position: absolute;
+                        top: 150px;
+                        left: 180px;
+                        widht: 100%;
+                        background-color: #fff;
+                        padding: 10px; 
+                        border: 1px solid #ccc;
+                        box-shadow: 0 4px 8px rgba(0,0,0,0.1);                          
+                    }
+
+                    .menuButton {
+                        display: none;
+                        background: transparent;
+                        font-size: 24px;
+                        margin-top: 20px;
+                        cursor: pointer;
+
+                    }
+
+                    .loginCadastro {                     
+                        margin-top: 40px;
+                        background: #FBBC04;
+                        border: 1px solid #ccc;
+                        padding: 6px 12px;
+                        border-radius: 4px;
+                        cursor: pointer;
+                        font-family: inherit;
+                    }
+
+                    loginCadastro:hover {
+                        background: #f0a8;
+                        border-top: 2px solid #333;
+                        border-right: 2px solid #333;
+                    }
+                    
+
+                    @media screen and (max-width: 768px) {
+                    
+                        .navbar { 
+                            flex-direction: column;
+                            gap: 10px;
+                            align-items: center;
+                        }
+
+                        .logo {
+                            width: 80%;
+                            height: auto;
+                        }
+                            
+                        .nav-items {
+                            display: none;
+                        }
+
+                        .nav-items.open {
+                            display: flex;
+                        }
+                            
+
+                        .menuButton {
+                            display: block;
+                            
+                        }
+
+                        .loginCadastro {
+                            Display: none;
+                        }
+                    } 
+                `}
+            </style>
+        </header>
+    );
+}
+
+export default NavegationBar;
+
+// https://www.youtube.com/watch?v=S5uQA-2NspY video para finalizar a navbar
