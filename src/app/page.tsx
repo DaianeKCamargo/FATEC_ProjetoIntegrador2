97a3bf0a319ac3d26ea3b59209059671de584e95
Sabrina Sant'Ana <sah.silva1d29@gmail.com>
2025-10-29 17:45:57 -0300

    att home
    att home doe tampinhas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Carrossel from "@/Components/Carrossel";
 import { Metadata } from "next"
 import Image from "next/image"; 
+import Link from "next/link";
 
 
 
@@ -15,17 +16,35 @@ export const metadata: Metadata = {
 export default function Home() {
     const imagens = ["/Procuro meu dono.png",
          "/ProcDono2.png", 
-         "/Procuro meu dono.png",];
+         "/ProcDono3.png",];
     return (
         <section>
             <div className="container-imagem">
                 <Image className="imagem" src="/home_TamPets.png" alt="Cachorro e Gato com Tampinhas" width={1920} height={1080} />
             </div>
             <div className="body">
-                    <h2 style={{ textAlign: "center", marginBottom: "20px", marginTop: "30px"}}>Procuro meu Dono</h2>
-                    <h3 style={{ textAlign: "center", marginBottom: "20px", marginTop: "30px"}}>Ajude a encontrar o lar desses animais</h3>
+                <div>
+                    <h1 style={{ textAlign: "center", marginBottom: "20px", marginTop: "30px"}}>Procuro meu Dono</h1>
+                    <h2 style={{ textAlign: "center", marginBottom: "20px", marginTop: "30px"}}>Ajude a encontrar o lar desses animais</h2>
                     <Carrossel imagens={imagens} />
+                </div>
+                <div>
+                    <h1  style={{ textAlign: "center", marginBottom: "20px", marginTop: "30px"}}> Doe suas Tampinhas! </h1>
+                    <h2  style={{ textAlign: "center", marginBottom: "20px", marginTop: "30px"}}>
+                        Ajude nossos amigos de quatro patas e com isso você ainda ajuda o meio ambiente.
+                    </h2>
+                </div>
+                <div style={{display: "flex",justifyContent: "center", alignItems: "center",height: "auto"}}>
+                     <Link href="/comoDoar">
+                     <Image className="imagem" src="/doacaotampinhas 1.png" alt="Doe" width={762} height={429}/>
+                     </Link>
+
+                </div>
+
             </div>
+            
+
+
 
 
         </section>
