ce14fe983bc027c256cd22aef6a96894cc7eacab
DaianeKCamargo <dkeelly@gmail.com>
2025-11-02 22:48:39 -0300

    faltou pastas ao importar no repositorio remoto

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import NavbarLogout from "@/components/navbar/navbarlogout";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const metadata: Metadata = {
+  title: "Bem-Vindos ao Projeto Tampets",
+  description: "Venha conhecer um pouco sobre nós e fazer parte desse projeto incrivel!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) 
+
+{
+  return (
+    <html lang="en">
+      <body>
+        <NavbarLogout />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+import styles from '@/styles/home.module.css';
+
+export default function Home() {
+  return(
+    <div>
+      <div className={styles.hero}>
+        <img className={styles.folder} src="/folder_home.png" alt="folder home" />
+      </div>
+      <div className={styles.parceiros}>
+
+      </div>
+    </div>
+  );
+}
