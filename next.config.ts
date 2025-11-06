104eda3afbfc6e215a5df073c00d61e860da9cd8
DaianeKCamargo <dkeelly@gmail.com>
2025-11-06 11:11:20 -0300

    navbar
    - ajuste de configurações para corrigir a falha ao carregar as imagens em git pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/nome-do-seu-repositorio", // o nome exato do repositório no GitHub
+  assetPrefix: "/nome-do-seu-repositorio/",
 };
 
 export default nextConfig;
