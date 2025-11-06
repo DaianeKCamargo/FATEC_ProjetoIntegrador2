ddcf77da515809abb1f8de9ff5c715f1bec95c3f
Sabrina Sant'Ana <sah.silva1d29@gmail.com>
2025-11-06 16:58:41 -0300

    public, app
    -Criação das pastas diretórias das pages tampets na midia, sobre nos e galeria
    -Upload das fotos das noticias

---
+++ app/sobre-nos/tampets-na-midia/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import "@/styles/namidia.module.css";
+
+
+export default function TamPetsNaMidia() {
+
+    return(
+        <section>
+           <div>
+                <h1>Tampets na Mídia</h1>
+                <h2>Confira aqui as notícias onde o projeto TamPets marcou presença!</h2>
+            </div>
+        </section>
+    );
+}
