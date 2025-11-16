621fced62146d36b0e14496b41a2463796ddf9cb
Sabrina Sant'Ana <sah.silva1d29@gmail.com>
2025-11-16 16:48:54 -0300

    galeria
    base galeria

--- app/sobre-nos/galeria-tampets/page.tsx
+++ app/sobre-nos/galeria-tampets/page.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import Card from "@/components/Card/Card";
+import Link from "next/link";
+import styles from "@/styles/galeria.module.css";
+
 export default function GaleriaTampets() {
     return(
         <section>
@@ -7,8 +11,42 @@ export default function GaleriaTampets() {
                 <h1>Galeria TamPets</h1>
                 <h2>Confira aqui os registros dos momentos TamPets!</h2>
             </div>
-            <div>
-                
+            <div className={styles["galeria-container"]}>
+                <Link href="./galeria-tampets/coletas">
+                    <Card
+                        imagem={"/GaleriaColetas.PNG"}
+                        titulo="Pontos de Coleta"
+                        descricao="Pontos de Coleta do Projeto!"
+                        className="card"
+                    />
+                </Link>
+
+                <Link href="./galeria-tampets/empresas">
+                    <Card
+                        imagem={"/GaleriaEmpresas.jpg"}
+                        titulo="Empresas"
+                        descricao="TamPets nas Empresas!"
+                        className="card"
+                    /> </Link>
+
+                <Link href="./galeria-tampets/eventos">
+                    <Card
+                        imagem={"/GaleriaEventos.PNG"}
+                        titulo="Eventos"
+                        descricao="Eventos do Projeto!"
+                        className="card"
+                    />
+                </Link>
+
+                <Link href="./galeria-tampets/escolas">
+                    <Card
+                        imagem={"/GaleriaEscolas.webp"}
+                        titulo="Escolas"
+                        descricao="Escolas que Participam!"
+                        className="card"
+                    />
+                </Link>
+
             </div>
         </section>
     );
