f63d9b0f6cd8a0efa9c7f9aa0cb07dc499d0d3ee
Sabrina Sant'Ana <sah.silva1d29@gmail.com>
2025-11-20 15:44:40 -0300

    adm galeria
    -front da page de adm da galeria tampets

--- app/administrativo/administrativo-galeria/page.tsx
+++ app/administrativo/administrativo-galeria/page.tsx
@@ -2,6 +2,9 @@
 
 import FileInput from "@/components/input-image/FileInput";
 import Select from "@/components/select/select";
+import style from "@/styles/adm-galeria.module.css";
+import { Button } from "react-bootstrap";
+
 
 
 export default function AdministrativoGaleria() {
@@ -16,24 +19,38 @@ export default function AdministrativoGaleria() {
   ];
 
     return (
-        <section>
+        <section className={style['section-page']}>
             <div>
-                <h1>Area do Adiminstrador</h1> 
+                <h1>Área do Adminstrador</h1> 
                 <h2>Galeria TamPets</h2>
             </div>
             <div>
+                <h3>PASSO 1: ESCOLHA DO ÁLBUM</h3>
+                <p>A galeria TamPets conta com 4 álbuns onde as fotos do projeto
+                    são exibidas. Escolha o álbum que deseja armazenar a imagem:
+                </p>
                 <Select
                     options={selectOptions}
-                    defaultValue="Escolha uma galeria"
+                    defaultValue="Álbuns Disponíveis"
                     onChange={handleChange}
                 />
             </div>
             <div>
+                <h3>PASSO 2: CARREGAMENTO DA IMAGEM</h3>
+                <p>Selecione a imagem que deseja enviar para o álbum escolhido:</p>
                 <FileInput 
-                    label="Insira a imagem desejada:"
+                    label="Insira aqui a imagem desejada:"
                     controlId="formFile"
                     onChange={(e) => console.log(e.target.files)}
                 />
+            </div>
+            <div>
+                <h3>PASSO 3: CONFIRMAÇÃO DO ENVIO</h3>
+                <p>Após fazer a escolha do album e da imagem desejados, confirme abaixo para finalizar o envio,
+                    ou cancele para reiniciar o processo:
+                </p>
+                <Button variant="primary">Enviar</Button>
+                <Button variant="secondary">Cancelar</Button>
             </div>
         </section>
         
