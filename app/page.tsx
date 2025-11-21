1f8c21b778a2dd2daf9d1ee5e0de994f3f7ff1f6
DaianeKCamargo <dkeelly@gmail.com>
2025-11-21 10:04:53 -0300

    FEAT: Home/doar
    inicialização da seção doar na página home.

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,9 @@
 
 import Image from "next/image";
 import styles from '@/styles/home.module.css';
-import { motion, AnimatePresence } from "framer-motion";
-import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
-
-  const [show, setShow] = useState(true);
-  const [turnIntoCoin, setTurnIntoCoin] = useState(false);
-
-  useEffect(() => {
-    const t1 = setTimeout(() => setTurnIntoCoin(true), 1500);
-    const t2 = setTimeout(() => setShow(false), 2300);
-    return () => {
-      clearTimeout(t1);
-      clearTimeout(t2);
-    };
-  }, []);
-
   return (
     <div>
 
@@ -27,7 +13,84 @@ export default function Home() {
       </div>
 
       <div className={styles.doar}>
-      </div> 
+        <div className={styles.left}>
+          <h2>Transforme tampinhas em boas ações</h2>
+          <p>
+            Cada tampinha vira impacto real. Veja a transição das tampinhas se
+            transformando em moedas, representando a ajuda que chega a quem
+            precisa.
+          </p>
+        </div>
+
+        <div className={styles.right}>
+          <motion.div
+            className="box"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+          >
+            {/* IMAGEM 1 */}
+            < motion.div
+              className="box"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}            >
+              <Image
+                src="/doar_tampinhas.png"
+                width={450}
+                height={450}
+                alt="Tampinhas caindo"
+              />
+            </motion.div>
+
+            {/* IMAGEM 2 */}
+            <motion.div
+              className="box"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+            >
+              <Image
+                src="/tampinhas_coin_caindo.png"
+                width={450}
+                height={450}
+                alt="Tampinhas se transformando em moedas"
+              />
+            </motion.div>
+
+            {/* IMAGEM 3 */}
+            <motion.div
+              className="box"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+            >
+              <Image
+                src="/coin.png"
+                width={450}
+                height={450}
+                alt="Mão segurando moedas"
+              />
+            </motion.div>
+          </motion.div>
+        </div>
+      </div >
 
       <div className={styles.pontocoleta}>
         <h1> Encontre o ponto de coleta mais próximo de você </h1>
@@ -45,6 +108,6 @@ export default function Home() {
         <h1> Parceiros </h1>
       </div>
 
-    </div>
+    </div >
   );
 }
